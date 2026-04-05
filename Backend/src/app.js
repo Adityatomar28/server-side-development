5f110dca826439682ff9a9a681df89f3f29524c5
Adityatomar28 <adityasinghtomar52@gmail.com>
2026-04-05 06:25:56 +0530

    Integrated the cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const uploadFile = require('./services/storage.service');
 const postModel = require('./models/post.model');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 //req.body m jo data aata tha usko json format m convert krne k liye middleware use krna padta hai taaki express pdh paay
 app.use(express.json());
 
